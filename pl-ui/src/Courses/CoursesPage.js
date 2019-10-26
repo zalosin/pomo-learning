6c13ae2efc03998afa74c73db6c6c9051d1321e5
Geo Emacu <gheorghe.emacu@adswizz.com>
2019-10-26 18:45:19 +0300

    Courses page

--- pl-ui/src/Courses/CoursesPage.js
+++ pl-ui/src/Courses/CoursesPage.js
@@ -1,7 +1,54 @@
-import React from 'react';
+import React, { Component, Fragment } from 'react';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import Fab from '@material-ui/core/Fab';
+import AddIcon from '@material-ui/icons/Add';
+import { Link as RouterLink } from 'react-router-dom';
 
-export default function() {
-    return (
-        <p>Courses page</p>
-    );
+import Api from '../Api';
+
+export default class CoursesPage extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            courses: [],
+        };
+    }
+
+    componentDidMount = () => {
+        Api.get('courses')
+            .then(json => {
+                this.setState({ courses: json });
+            });
+    }
+
+    render() {
+        const { courses } = this.state;
+        const fabStyle= { position: 'fixed', bottom: '50px', right: '50px' };
+
+        return (
+            <Fragment>
+                <List>
+                    {courses.map(course => (
+                        <RouterLink to={`/courses/${course.id}`}>
+                            <ListItem divider>
+                                <ListItemText
+                                    primary={course.title}
+                                    secondary={course.description}
+                                />
+                            </ListItem>
+                        </RouterLink>
+                    ))}
+                    
+                    <RouterLink to="/createCourse">
+                        <Fab color="primary" style={fabStyle} >
+                            <AddIcon />
+                        </Fab>
+                    </RouterLink>
+                </List>
+            </Fragment>
+        );
+    }
 }
