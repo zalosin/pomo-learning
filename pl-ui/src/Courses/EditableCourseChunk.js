d72d19d889fe44666eb4f6008f860f3a9e27a090
Ionuț-Cătălin Tătuță <i.c.tatuta@gmail.com>
2019-10-26 18:49:31 +0300

    Fix conflict and add single course page

--- pl-ui/src/Courses/EditableCourseChunk.js
+++ pl-ui/src/Courses/EditableCourseChunk.js
@@ -1,28 +1,60 @@
 import React, {Component} from 'react';
-import PropTypes from 'prop-types';
 import {stateToHTML} from "draft-js-export-html";
-import {convertFromRaw} from "draft-js";
-import MUIRichTextEditor from "../RTETestPage";
+import {EditorState, convertFromRaw, convertToRaw, ContentState} from "draft-js";
+import MUIRichTextEditor from "mui-rte";
+import EditIcon from '@material-ui/icons/Edit';
+import Button from '@material-ui/core/Button';
 
 class EditableCourseChunk extends Component {
     constructor(props) {
         super(props);
+        const emptyEditorState = EditorState.createEmpty();
         this.state = {
-            editorState: props.editorState || null
+            editMode: false,
+            contentState: props.contentState || emptyEditorState.getCurrentContent()
         }
     }
-    render() {
+
+    onSave = (contentState) => {
+        this.setState({
+            editMode: false,
+            contentState,
+            defaultDataLoaded: false
+        })
+    };
+
+    renderEditorDataHTML = () => {
+        const {contentState} = this.state;
         return (
-            <MUIRichTextEditor
-                label="Hello welcome to course editor"
-                onSave={data=>{
-                    console.log(stateToHTML(convertFromRaw(JSON.parse(data))))
-                }}
-            />
+            <div>
+                <Button
+                    variant="contained"
+                    color="primary"
+                    onClick={()=>this.setState({editMode: true})}
+                >
+                    <EditIcon />
+                </Button>
+                <div dangerouslySetInnerHTML={{__html: stateToHTML(contentState)}} />
+            </div>
+        )
+    };
+
+    render() {
+        const {editMode, contentState, defaultDataLoaded} = this.state;
+        const rawDraftContentState = JSON.stringify(convertToRaw(contentState));
+
+        return (
+            <div>
+                {editMode ? (
+                    <MUIRichTextEditor
+                        label="Start typing..."
+                        value={(!defaultDataLoaded) ? rawDraftContentState : undefined}
+                        onSave={data=>this.onSave(convertFromRaw(JSON.parse(data)))}
+                    />
+                ) : this.renderEditorDataHTML()}
+            </div>
         );
     }
 }
-
-EditableCourseChunk.propTypes = {};
 
 export default EditableCourseChunk;
