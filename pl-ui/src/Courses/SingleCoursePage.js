beda1419592fb4c779cc21d74d04f23fbe3ba8bf
Ionuț-Cătălin Tătuță <i.c.tatuta@gmail.com>
2019-10-27 14:56:34 +0200

    timer style fix

--- pl-ui/src/Courses/SingleCoursePage.js
+++ pl-ui/src/Courses/SingleCoursePage.js
@@ -253,6 +253,7 @@ class SingleCoursePage extends Component {
                                     padding: "10px",
                                     fontSize: "30px",
                                     boxShadow: "0px 3px 5px -1px rgba(0,0,0,0.2), 0px 5px 8px 0px rgba(0,0,0,0.14), 0px 1px 14px 0px rgba(0,0,0,0.12)",
+                                    backgroundColor:"#fff"
                                 }}
                             >
                                 <Timer.Minutes />:<Timer.Seconds />
@@ -281,8 +282,8 @@ class SingleCoursePage extends Component {
                                     border: "2px solid #000",
                                     padding: "10px",
                                     fontSize: "30px",
-                                    background: "white",
                                     boxShadow: "0px 3px 5px -1px rgba(0,0,0,0.2), 0px 5px 8px 0px rgba(0,0,0,0.14), 0px 1px 14px 0px rgba(0,0,0,0.12)",
+                                    backgroundColor:"#fff"
                                 }}
                             >
                                 <Timer.Minutes />:<Timer.Seconds />
