060401d740d91b725abd225ac4631470899cac71
Ionuț-Cătălin Tătuță <i.c.tatuta@gmail.com>
2019-10-27 03:04:18 +0300

    Move backend back on Port 3000

--- pl-api/index.js
+++ pl-api/index.js
@@ -3,7 +3,7 @@
  */
 const express = require("express");
 const app = express();
-const port = 3001;
+const port = 3000;
 const bodyParser = require("body-parser");
 const cors = require("cors");
 /**
